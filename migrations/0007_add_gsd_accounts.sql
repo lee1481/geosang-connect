86f72c02f3079262849e015b624fa678054d7e67
lee1481 <genspark_dev@genspark.ai>
2026-01-31 08:36:15 +0000

    feat: gsd2026, gsd1481 로그인 계정 추가
    - authorized_users 테이블에 gsd2026, gsd1481 계정 추가
    - 마이그레이션 파일 0007_add_gsd_accounts.sql 생성

---
+++ migrations/0007_add_gsd_accounts.sql
@@ -0,0 +1,5 @@
+-- gsd2026, gsd1481 계정 추가
+INSERT OR IGNORE INTO authorized_users (id, name, username, password)
+VALUES 
+  ('gsd2026', '거상 사용자 2026', 'gsd2026', 'gsd2026'),
+  ('gsd1481', '거상 사용자 1481', 'gsd1481', 'gsd1481');
